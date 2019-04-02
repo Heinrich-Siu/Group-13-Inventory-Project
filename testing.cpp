c6a1fe439e5d3522f3a40e2ad9113265fe28a1e6
marco9663 <u3555358@hku.hk>
2019-04-02 23:25:34 +0800

    justing testing

--- testing.cpp
+++ testing.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <sstream>
 #include "inventorysys.h"
 //#include "inventorysys.cpp"
 
@@ -26,5 +27,43 @@ int main()
     apple.name = "Japan Apple";
     //cout << apple.index << endl;
     //cout << apple.name << endl;
-    printUI();
+    //printUI();
+
+    string line = "11,22,33,44,55,66,77";
+    int lineArrIndicator = 0;
+
+    //getline(myfile,type);
+    istringstream ss(line);
+    string temp;
+    while(getline(ss,temp,','))
+    {
+         cout<<temp<<endl;
+    }
+
+    /*string currentWord = "";
+    for(int j=0; j<line.length(); j++)  //loop through all character
+    {
+      if(line[j]== ',') //check if the character is alpha
+      {
+        cout << currentWord; //store full word to array
+        currentWord = ""; //restore to initial state
+        lineArrIndicator++;
+        //lineArr[lineArrIndicator] = line[j];
+        cout << " ";
+        lineArrIndicator++;
+      }
+      else
+      {
+        currentWord += line[j]; //append character to word string
+      }
+      if(j==line.length()-1 && isalpha(line[j])) //for final character in the line
+      {
+        cout << currentWord; //store full word to array
+        //lineArrIndicator++;
+
+      }
+
+    }*/
+
+
 }
