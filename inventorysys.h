f6724ee1e7344a9ce7029ad7e2f11508992005f8
Heinrich-Siu-KH <47171716+Heinrich-Siu@users.noreply.github.com>
2019-04-06 14:01:36 +0800

    Update inventorysys.h

--- inventorysys.h
+++ inventorysys.h
@@ -4,6 +4,17 @@
 #include <string.h>
 #include <time.h>
 
+/*
+tm_sec	int	seconds after the minute	0-60*
+tm_min	int	minutes after the hour	0-59
+tm_hour	int	hours since midnight	0-23
+tm_mday	int	day of the month	1-31
+tm_mon	int	months since January	0-11
+tm_year	int	years since 1900	
+tm_wday	int	days since Sunday	0-6
+tm_yday	int	days since January 1	0-365
+*/
+
 struct salesRecord
 {
     tm date;
