1d47e9a186d74a263cba8e9cd60191380de2ba53
Heinrich-Siu-KH <47171716+Heinrich-Siu@users.noreply.github.com>
2019-04-18 19:42:42 +0800

    Add functions
    Migrated some functions from controlrecord.cpp for easier magament

--- printer.cpp
+++ printer.cpp
@@ -18,7 +18,7 @@ void inventoryShower(commodity * shopPtr, int numberOfCommodity);
 
 void optionPrinter(int i){ //print the columns
     switch (i) {
-            //"1. Index 2.ProductCode 3.Name 4.Price 5.StockNum 6.StockSize 7.NumOfSalesRec 8.numOfRestockRec"
+            //"1. Index 2.Product Code 3.Name 4.Price 5.Stock Number \n6.Stock Size 7.Number Of Sales Record 8.Number Of Restock Record 9.Manufacturer 10.Tax Amount\n"
         case 1:
             cout.width(10); cout<<left<<"Index";
             break;
@@ -41,16 +41,21 @@ void optionPrinter(int i){ //print the columns
             cout.width(23); cout<<left<<"Number of Sales Record";
             break;
         case 8:
-            cout.width(24); cout<<left<<"Number of Restock Record";
+            cout.width(26); cout<<left<<"Number of Restock Record";
             break;
-
+        case 9:
+            cout.width(15); cout<<left<<"Manufacturer";
+            break;
+        case 10:
+            cout.width(15); cout<<left<<"Tax Amount";
+            break;
         default:
             break;
     }
 }
 void specPrinter(int i, commodity * product){ //print product info according to request
     switch (i) {
-            //"1. Index 2.ProductCode 3.Name 4.Price 5.StockNum 6.StockSize 7.NumOfSalesRec 8.numOfRestockRec"
+            //"1. Index 2.Product Code 3.Name 4.Price 5.Stock Number \n6.Stock Size 7.Number Of Sales Record 8.Number Of Restock Record 9.Manufacturer 10.Tax Amount\n"
         case 1:
             cout.width(10); cout<<left<<product->index;
             break;
@@ -73,9 +78,14 @@ void specPrinter(int i, commodity * product){ //print product info according to 
             cout.width(23); cout<<left<<product->numOfSalesRec;
             break;
         case 8:
-            cout.width(24); cout<<left<<product->numOfRestockRec;
+            cout.width(26); cout<<left<<product->numOfRestockRec;
             break;
-
+        case 9:
+            cout.width(15); cout<<left<<product->manufacturer;
+            break;
+        case 10:
+            cout<<"$"; cout.width(14); cout<<left<<product->taxAmount;
+            break;
         default:
             break;
     }
@@ -89,22 +99,32 @@ void recordPrinter(commodity * product, int * fields){
 }
 
 void inventoryShower(commodity * shopPtr, int numberOfCommodity){ //"1. Show all inventory record\n"
-    cout<<"Choose what to show in sequence and enter -1 when done\n"<<"Here are the options: \n";
+    cout<<"Input the number one by one in any sequence and input -1 when done or enter 11 to print All\n"<<"***e.g. 1 2 5 4 -1***"<<endl;
+    cout<<"Here are the options: \n";
     cout<<"1. Index 2.Product Code 3.Name 4.Price 5.Stock Number \n6.Stock Size 7.Number Of Sales Record 8.Number Of Restock Record 9.Manufacturer 10.Tax Amount 11.ALL\n";
     int* fields = new int[11]; //dynamic array that store the aspects need to print
-    int a=0, all=-1;
+    int a=0, all=0;
     for (int k=0; a!=-1; k++) { //load in different aspects from user
         cin>>a;
-        if (a==11)all=1; break;
+        if (a==11){
+            all=1;
+            break;
+        }
         fields[k] = a;
     }
     if (all){//print all
-        for (int i=0; i<11; i++) fields[i]=i;
+        for (int i=0; i<11; i++){
+            fields[i]=i;
+        }
         fields[11]=-1;
     }
-    for (int i=0; (fields[i])!=-1; i++) optionPrinter(fields[i]); //print the columns
+    
+    for (int i=0; (fields[i])!=-1 ; i++) {
+        optionPrinter(fields[i]); //print the columns
+    }
     cout<<endl;
-    for (int i=0; i<numberOfCommodity; i++) recordPrinter(shopPtr+i,fields); //print the data one by one
+    for (int i=0; i<numberOfCommodity; i++)
+        recordPrinter(shopPtr+i,fields); //print the data one by one
     delete [] fields;
 }
 
@@ -147,11 +167,26 @@ void restockRecordPrinter(restockRecord *restockRec, int numOfRestockRec)
 
 void recordPrinterByIndex(commodity * shopPtr, int index)
 {
-  for (int i=1; i<=9; i++)
+  for (int i=1; i<=10; i++)
       optionPrinter(i); //print the columns
 
   cout<<endl;
 
-  for (int i=1; i<=9; i++)
+  for (int i=1; i<=10; i++)
       specPrinter(i, shopPtr+index); //print the data in structure one by one
+  cout<<endl;
+}
+
+//Prompt when asking user to change Record
+void recordChangePrompt(){
+    cout << "**************************************************\n"
+    "*~~~What record do you want to change/update?    *\n"
+    "*1. Product Code                                 *\n"
+    "*2. Product Name                                 *\n"
+    "*3. Price                                        *\n"
+    "*4. Size of stock                                *\n"
+    "*5. Tax amount                                   *\n"
+    "*6. Name of manufacturer                         *\n"
+    "*7. Quit                                         *\n"
+    "**************************************************" << endl;
 }
