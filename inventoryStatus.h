45cf2509a2710c54213dc9017de4ccfc6fc10e69
Heinrich-Siu-KH <47171716+Heinrich-Siu@users.noreply.github.com>
2019-04-24 20:46:33 +0800

    update to delete item

--- inventoryStatus.h
+++ inventoryStatus.h
@@ -2,11 +2,14 @@
 #define inventoryStatus_h
 #include "inventorysys.h"
 #include <stdio.h>
+
 void stockPrinter(commodity *product);
-void salesAverage(commodity *shopPtr, int index, tm fromDate, int numOfMonth, double &average);
-void tdayStrtoInt(std::string date, tm &fromDate);
+double salesAmount(commodity *shopPtr, int index, tm fromDate, int numOfMonth);
+void tdayStrtoInt(string date, tm &fromDate);
 void salesPrediction(commodity* shopPtr, int numberOfCommodity);
 void stockStatus(commodity *shopPtr, int numberOfCommodity);
-void checkInventoryStatus(commodity* shopPrt, int numberOfCommodity);
+void reduce_commodityRecord(commodity *&shopPtr, int originSize, int target);
+void deleteOutDatedCommodity(commodity *& shopPtr, int &numberOfCommodity);
+void checkInventoryStatus(commodity*&shopPrt, int &numberOfCommodity);
 
 #endif /* inventoryStatus_h */
