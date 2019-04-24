41cd65a0a5b9942b9f80bccc1abeacef7db41667
Heinrich-Siu-KH <47171716+Heinrich-Siu@users.noreply.github.com>
2019-04-24 18:53:27 +0800

    change for status menu

--- inventoryStatus.h
+++ inventoryStatus.h
@@ -2,9 +2,11 @@
 #define inventoryStatus_h
 #include "inventorysys.h"
 #include <stdio.h>
-void stockPrinter(commodity *product, double predictedSales);
-void salesAverage(commodity *shopPtr, int index, tm fromDate, int numOfMonth);
+void stockPrinter(commodity *product);
+void salesAverage(commodity *shopPtr, int index, tm fromDate, int numOfMonth, double &average);
 void tdayStrtoInt(std::string date, tm &fromDate);
+void salesPrediction(commodity* shopPtr, int numberOfCommodity);
+void stockStatus(commodity *shopPtr, int numberOfCommodity);
 void checkInventoryStatus(commodity* shopPrt, int numberOfCommodity);
 
 #endif /* inventoryStatus_h */
