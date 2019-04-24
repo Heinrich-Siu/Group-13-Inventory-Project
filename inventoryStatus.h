12c0213c60d27eb7328dc8ecd1c22e265a994f75
Heinrich-Siu-KH <47171716+Heinrich-Siu@users.noreply.github.com>
2019-04-24 20:47:06 +0800

    forgot std::

--- inventoryStatus.h
+++ inventoryStatus.h
@@ -5,7 +5,7 @@
 
 void stockPrinter(commodity *product);
 double salesAmount(commodity *shopPtr, int index, tm fromDate, int numOfMonth);
-void tdayStrtoInt(string date, tm &fromDate);
+void tdayStrtoInt(std::string date, tm &fromDate);
 void salesPrediction(commodity* shopPtr, int numberOfCommodity);
 void stockStatus(commodity *shopPtr, int numberOfCommodity);
 void reduce_commodityRecord(commodity *&shopPtr, int originSize, int target);
